c25038101993fbc13b6aea8c311f87c1c0c30f25
CpBruceMeena <sandeep10198@gmail.com>
2020-01-04 12:41:35 +0530

    Adding posts to the home page using axios library

--- poketimes/src/components/home.js
+++ poketimes/src/components/home.js
@@ -1,12 +1,46 @@
-import React from 'react'
+import React, { Component } from 'react'
+import axios from 'axios'
 
-const Home = () => {
-  return (
-    <div className='container'>
-      <h1 className='center'>Home</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eu mauris magna. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque vitae tincidunt metus. Vivamus dignissim congue sagittis. Duis at enim laoreet, facilisis arcu aliquet, varius ligula. Suspendisse consequat viverra purus, vitae vestibulum dui lobortis ut. Curabitur varius scelerisque ullamcorper. Quisque mi eros, gravida vitae purus eget, tincidunt vehicula risus. Donec rutrum ex eu commodo dictum. In hendrerit elit vel orci porta congue. Aliquam volutpat quam in ante dapibus rutrum. Duis ac libero tempus magna sagittis rutrum et at lacus. Curabitur et dui sed dolor hendrerit suscipit. Etiam tempor ligula vel sapien commodo elementum.</p>
-    </div>
-  )
+class Home extends Component {
+  state = {
+    posts: []
+  }
+
+  componentDidMount(){
+    axios.get("https://jsonplaceholder.typicode.com/posts")
+      .then(res=>{
+        this.setState({
+          posts: res.data.slice(0, 10)
+        })
+      })
+  }
+
+  render(){
+
+    const {posts} = this.state;
+    const postList = posts.length ? (
+      posts.map(post=>{
+        return(
+          <div className="post card" key={post.id}>
+            <div className="card-content">
+              <span className='card-title'>{post.title}</span>
+              <p>{post.body}</p>
+            </div>
+          </div>
+        )
+      })
+    ) : (
+      <div>No posts yet</div>
+    )
+
+
+    return (
+      <div className='container'>
+        <h1 className='center'>Home</h1>
+        {postList}
+      </div>
+    )
+  }
 }
 
 export default Home
