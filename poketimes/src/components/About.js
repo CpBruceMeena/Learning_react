fe24016d1c0ffb9288daecc00bc57c8b9b8762e5
CpBruceMeena <sandeep10198@gmail.com>
2020-01-04 12:16:55 +0530

    Using router for an app 'poketimes'

---
+++ poketimes/src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Rainbow from '../hoc/Rainbow'
+
+const About = () => {
+  return (
+    <div className='container'>
+    <h1 className='center'>About</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eu mauris magna. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque vitae tincidunt metus. Vivamus dignissim congue sagittis. Duis at enim laoreet, facilisis arcu aliquet, varius ligula. Suspendisse consequat viverra purus, vitae vestibulum dui lobortis ut. Curabitur varius scelerisque ullamcorper. Quisque mi eros, gravida vitae purus eget, tincidunt vehicula risus. Donec rutrum ex eu commodo dictum. In hendrerit elit vel orci porta congue. Aliquam volutpat quam in ante dapibus rutrum. Duis ac libero tempus magna sagittis rutrum et at lacus. Curabitur et dui sed dolor hendrerit suscipit. Etiam tempor ligula vel sapien commodo elementum.</p>
+    </div>
+  )
+}
+
+export default Rainbow(About)
