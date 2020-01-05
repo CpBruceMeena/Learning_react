c49a298205c72e5f78746ad2d1544f26105cef08
CpBruceMeena <sandeep10198@gmail.com>
2020-01-05 12:43:48 +0530

    Dispatching actions and final set up

--- poketimes/src/reducers/rootReducer.js
+++ poketimes/src/reducers/rootReducer.js
@@ -1,22 +1,32 @@
 const initState= {
   posts: [{
-    "id": 1,
-    "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-    "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+    id: "1",
+    title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+    body: "Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book"
   },
   {
-    "id": 2,
-    "title": "qui est esse",
-    "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+    id: "2",
+    title: "qui est esse",
+    body: "Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book"
   },
   {
-    "id": 3,
-    "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
-    "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
+    id: "3",
+    title: "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+    body: "Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book"
   }]
 }
 
 const rootReducer = (state = initState, action) => {
+  if (action.type === 'DELETE_POST'){
+    let newPosts = state.posts.filter(post => {
+      return action.id !== post.id
+    });
+    return {
+      ...state,
+      posts: newPosts
+    }
+  }
+
   return state;
 }
 
